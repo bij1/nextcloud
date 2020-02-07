1e45b5075fc678bb81809147abfb3ba8e9047afc
Arjan Adriaanse <arjan@adriaan.se>
2020-02-07 19:14:16 +0100

    initial commit

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+conf() {
+    docker-compose exec --user www-data app php occ config:system:set "$1" --value "$2"
+}
+
+conf theme bij1
+conf default_language nl
+conf default_locale nl_NL
